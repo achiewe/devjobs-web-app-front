64b0b2b91769e5a23d00124fdd8825c773b2b19a
achiewe <mamukashviliachi706@gmail.com>
2023-08-13 18:23:51 +0400

    add styles for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { styled } from "styled-components";
 import GlobalStyles from "./GlobalStyles";
 function App() {
   return (
-    <div>
+    <MainContainer>
       <GlobalStyles />
-    </div>
+    </MainContainer>
   );
 }
+
+const MainContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f4f6f8;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
 
 export default App;
