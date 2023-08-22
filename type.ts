f96394961b4f6bdaf792b31ca2b36794df99f517
achiewe <mamukashviliachi706@gmail.com>
2023-08-22 18:31:08 +0400

    add new file types where i write data json info type

---
+++ type.ts
@@ -0,0 +1,17 @@
+interface Jobs {
+  id: number;
+  company: string;
+  logo: string;
+  logoBackground: string;
+  position: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  website: string;
+  apply: string;
+  description: string;
+  requirements: { content: string; items: string };
+  role: { content: string; items: string };
+}
+
+export default Jobs;
