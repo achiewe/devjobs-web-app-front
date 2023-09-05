4d60ae0c90249edd3eaaa9308ba2d7531ab186f2
achiewe <mamukashviliachi706@gmail.com>
2023-09-05 13:02:58 +0400

    add it in redux.ts to use it

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -1,17 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ThemeSlice, { modeProps } from "./ModeSlice";
 import FiltFrameSlice, { filtProps } from "./FiltFrameSlice";
+import DevJobsSlice, { devProps } from "./DevJobsSlice";
 
 const store = configureStore({
   reducer: {
     Mode: ThemeSlice,
     FiltFrame: FiltFrameSlice,
+    DevJob: DevJobsSlice,
   },
 });
 
 export type Mode = {
   Mode: modeProps;
   FiltFrame: filtProps;
+  DevJob: devProps;
 };
 
 export default store;
