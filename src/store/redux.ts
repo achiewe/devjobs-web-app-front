5efa00789b909a7054c2292e871ee802461b9e32
achiewe <mamukashviliachi706@gmail.com>
2023-09-05 16:18:43 +0400

    add function in redux.ts to use it

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import ThemeSlice, { modeProps } from "./ModeSlice";
 import FiltFrameSlice, { filtProps } from "./FiltFrameSlice";
 import DevJobsSlice, { devProps } from "./DevJobsSlice";
+import EveryJobSlice, { EveryProps } from "./EveryJobSlice";
 
 const store = configureStore({
   reducer: {
     Mode: ThemeSlice,
     FiltFrame: FiltFrameSlice,
     DevJob: DevJobsSlice,
+    EveryJob: EveryJobSlice,
   },
 });
 
@@ -15,6 +17,7 @@ export type Mode = {
   Mode: modeProps;
   FiltFrame: filtProps;
   DevJob: devProps;
+  EveryJob: EveryProps;
 };
 
 export default store;
